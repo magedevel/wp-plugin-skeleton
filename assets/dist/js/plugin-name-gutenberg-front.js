cd9e4f2748fdf2740f8d11e96f87c287ee47ad05
Dave Green <hello@davetgreen.me>
2019-01-27 16:44:25 +0000

    Refactor WIP.

--- assets/dist/js/plugin-name-gutenberg-front.js
+++
@@ -1,6 +0,0 @@
-( function( $ ) {
-	'use strict';
-
-	// Gutenberg front Javascript.
-
-} )( jQuery );
--- assets/src/js/plugin-name-gutenberg-editor.js
+++
@@ -1,6 +0,0 @@
-( function( $ ) {
-	'use strict';
-
-	// Gutenberg editor Javascript.
-
-} )( jQuery );
